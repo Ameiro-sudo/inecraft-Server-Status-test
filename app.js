6f7b88910f50150d520633b0ba1e80c89001a360
ninasukiwww-png <ninasukiwww@gmail.comm>
2026-06-05 22:24:49 +0800

    Remove cache/ SW, darken bg overlay

--- app.js
+++ app.js
@@ -15,16 +15,6 @@
         }, dur);
     }
 
-    if ('serviceWorker' in navigator) {
-        window.addEventListener('load', function() {
-            navigator.serviceWorker.register('/sw.js').then(function(reg) {
-                console.log('SW registered:', reg.scope);
-            }).catch(function(err) {
-                console.log('SW registration failed:', err);
-            });
-        });
-    }
-
     var chartModal = document.getElementById('chartModal');
     var modalTitle = document.getElementById('modalTitle');
     var modalBody = document.getElementById('modalBody');
